bb4d6e10aef005236696d00ea6a41af7b5aa5e8a
abyanmajid <abyan@abydyl.net>
2024-04-05 01:16:00 +1100

    delete: sb connection

--- server/Program.cs
+++ server/Program.cs
@@ -35,33 +35,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-/**/
-/* app.MapPost("/boards", async ( */
-/*       CreateBoardRequest request, */
-/*       Supabase.Client supabase) => */
-/*     { */
-/*       var board = new Board { */
-/*         Name = request.Name, */
-/*         Members = request.Members, */
-/*         Tasks = request.Tasks, */
-/*       }; */
-/**/
-/*       var response = await supabase.From<Board>().Insert(board); */
-/*       var newBoard = response.Models.First(); */
-/*       return Results.Ok(newBoard.Id); */
-/*     } */
-/* ); */
 
 app.Run();
-
-// "/": Hello, World!
-// "/board": Get all boards (GET)
-// "/board": Create a new board (POST)
-// "/board/<id>": Get board by id (GET)
-// "/board/<id>": Update board by id (PUT)
-// "/board/<id>": Delete board by id (DELETE)
-// "/tasks": Get all tasks (GET)
-// "/tasks": Create a new task (POST)
-// "/tasks/<id>": Get task by id (GET)
-// "/tasks/<id>": Update task by id (PUT)
-// "/tasks/<id>": Delete task by id (DELETE)
